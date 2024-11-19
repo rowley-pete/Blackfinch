74851988272e838427dabfeac6a3b4df1a491330
Peter Rowley <rowley.pete@live.co.uk>
2024-11-19 23:38:29 +0000

    Updated constructor for RejectRule

--- Blackfinch/RejectRule.cs
+++ Blackfinch/RejectRule.cs
@@ -1,6 +1,6 @@
 namespace Blackfinch;
 
-public class RejectRule(IApplicationRule next) : ApplicationRule(next)
+public class RejectRule(IApplicationRule? next) : ApplicationRule(next)
 {
   public override bool? Process(decimal loanAmount, decimal assetValue, int creditScore)
   {
